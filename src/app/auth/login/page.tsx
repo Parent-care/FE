db10b0e24d3e3deb99ff935ac122dd2f07247716
safiratun <mc256d5x1807@student.devacademy.id>
2025-06-14 06:06:18 +0700

    Update page.tsx

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -23,7 +23,7 @@ export default function LoginPage() {
     e.preventDefault();
 
     try {
-      const res = await fetch('api/auth/login', {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/auth/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
