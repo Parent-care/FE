85bcf78c168d2ef275c1e5acecdb10a27889238e
safiratun <mc256d5x1807@student.devacademy.id>
2025-06-14 06:28:52 +0700

    perubahan

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -114,7 +114,7 @@ console.log('Response Body:', result);
             {/* Header */}
             <div className="text-center mb-8">
               <h2 className="text-3xl font-bold text-gray-800 mb-2">Log in</h2>
-              <p className="text-gray-600">Masuk ke akun ParentCare Anda Bro</p>
+              <p className="text-gray-600">Masuk ke akun ParentCare Anda Brok</p>
             </div>
 
             {/* Form */}
