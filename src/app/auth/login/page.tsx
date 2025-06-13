665c6c163895fc9fbf7272bc58fe518d49c98e05
safiratun <mc256d5x1807@student.devacademy.id>
2025-06-14 05:46:32 +0700

    Update page.tsx

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -23,7 +23,7 @@ export default function LoginPage() {
     e.preventDefault();
 
     try {
-      const res = await fetch('https://be-production-0885.up.railway.app/api/auth/login', {
+      const res = await fetch('api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
