cdd46dc2b327b5addcd2a1bbd508fd1a8b08f0c9
safiratun <mc256d5x1807@student.devacademy.id>
2025-06-15 07:07:17 +0700

    update one

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "gnews.io",
+      "cdn.cnn.com",
+      "static01.nyt.com",
+      "ichef.bbci.co.uk",
+      "media.cnn.com",
+      "assets.bwbx.io",
+      "media.npr.org",
+      "m.media-amazon.com"
+    ],
+  },
 };
 
 export default nextConfig;
