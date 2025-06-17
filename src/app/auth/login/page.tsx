edc901b84f2691be35dadc31f11a112ead4e923b
safiratun <mc256d5x1807@student.devacademy.id>
2025-06-17 14:35:40 +0700

    update one

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -182,9 +182,6 @@ console.log('Response Body:', result);
                 <div className="absolute inset-0 flex items-center">
                   <div className="w-full border-t border-gray-200"></div>
                 </div>
-                <div className="relative flex justify-center text-sm">
-                  <span className="px-2 bg-white text-gray-500">or log in with</span>
-                </div>
               </div>
 
               {/* Register Link */}
